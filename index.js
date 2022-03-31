64ca79bafb1c731a68d3cdf86ed2e879eb08c0cc
Yivan2021 <manuel.rodriguez@softwareestrategico.com>
2022-03-31 15:59:34 -0500

    ciclos

---
+++ index.js
@@ -0,0 +1,25 @@
+
+const miPrimerArray = ['pedro', 'hola', 1, true, 555, { "nombre": 'pepito'}, 'holasss'];
+
+console.log(miPrimerArray.length);
+
+
+
+for(let contador =0; contador <2; contador++) {
+    console.log( ` posicion de miPrimerArray[${contador}] = `)
+    console.log(miPrimerArray[contador]);
+
+}
+
+
+// let i=1;
+// do {
+//     console.log('hola ', i)
+//     i=i+1;
+// } while (i <= 10);
+
+// let a= 1;
+// while (a< 10) {
+//     a=a+1;
+//     console.log('hola ', a);
+// }
